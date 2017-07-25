282ed7fc0c1d1678dc3b26aa7266f8478ba99110
chriddyp <chris@plot.ly>
2017-07-25 14:13:58 -0400

    fix tests

--- tests/test_plotlyauth.py
+++ tests/test_plotlyauth.py
@@ -6,7 +6,9 @@ import dash_html_components as html
 import six
 from six.moves import http_cookies
 from six import iteritems
-
+import dash_auth
+from dash_auth import plotly_auth
+from dash_auth.plotly_auth import PlotlyAuth
 
 if six.PY3:
     from unittest import mock
@@ -34,30 +36,54 @@ users = {
 endpoints = {
     'protected': {
         'get': [
-            '/layout', '/routes', '/dependencies',
+            '/_dash-layout', '/_dash-routes', '/_dash-dependencies',
+            '/_dash-component-suites/dash_html_components/bundle.js'
         ],
-        'post': ['/update-component']
+        'post': ['/_dash-update-component']
     },
     'unprotected': {
-        'get': [
-            '/', '/_config', '/component-suites/dash-html-components'
-        ],
-        'post': [
-            '/_login'
-        ]
+        'get': ['/', '/some-url'],
+        'post': []
     }
 }
-n_protected_endpoints = len(endpoints['protected']['get'])
+
 
 
 def get_cookie(res, cookie_name):
     headers = res.headers.to_list()
-    cookie_string = [h for h in headers if (
+    set_cookie_strings = [h for h in headers if (
         h[0] == 'Set-Cookie' and cookie_name in h[1]
-    )][0][1]
+    )]
+    try:
+        cookie_string = set_cookie_strings[0][1]
+    except IndexError as e:
+        print(cookie_name)
+        for header in headers:
+            print(header)
+        print(set_cookie_strings)
+        raise e
+
     cookie = http_cookies.SimpleCookie(cookie_string)
     access_granted_cookie = cookie[list(cookie.keys())[0]].value
     return access_granted_cookie
+
+
+def create_apps():
+    app_permissions = ['public', 'private']
+    apps = {k: dash.Dash(k) for k in app_permissions}
+    for app in apps.values():
+        app.scripts.config.serve_locally = True
+    auths = {
+        k: PlotlyAuth(
+            apps[k],
+            '{}-app-test'.format(k),
+            k,
+            'http://localhost:5000'
+        ) for k in app_permissions
+    }
+    apps['unregistered'] = dash.Dash('unregistered')
+    apps['unregistered'].scripts.config.serve_locally = True
+    return apps, auths
 
 
 class ProtectedViewsTest(unittest.TestCase):
@@ -68,56 +94,52 @@ class ProtectedViewsTest(unittest.TestCase):
         )
         self.longMessage = True
 
-    def create_apps(self):
-        auths = {
-            k: PlotlyAuth(
-                dash.Dash(k),
-                '{}-app-test'.format(k),
-                'public',
-                'http://localhost:5000'
-            ) for k in ['public', 'private']
-        }
-        apps = {auth.app.__name__: auth.app for auth in auths}
-        apps['unregistered'] = dash.Dash('unregistered')
-        return apps, auths
 
     def test_protecting_all_views(self):
-        self.assertEqual(
+        apps = create_apps()[0]
+        self.assertEqual((
             len(endpoints['protected']['get']) +
             len(endpoints['unprotected']['get']) +
             len(endpoints['protected']['post']) +
-            len(endpoints['unprotected']['post']),
-            len(self.apps['unregistered'].view_functions)
+            len(endpoints['unprotected']['post'])),
+            len([k for k in apps['unregistered'].server.url_map.iter_rules()])
         )
 
     def test_unauthenticated_view(self):
-        apps = self.create_apps()
-        for app in [apps['unregistered'], apps['public']]:
+        apps = create_apps()[0]
+        for app_name in ['unregistered']:
+            app = apps[app_name]
             app.layout = html.Div()
             client = app.server.test_client()
             for endpoint in (endpoints['protected']['get'] +
                              endpoints['unprotected']['get']):
                 res = client.get(endpoint)
-                self.assertEqual(res.status_code, 200)
+                test_name = '{} at {} ({})'.format(
+                    res.status_code, endpoint, app_name
+                )
+
+                self.assertEqual(res.status_code, 200, test_name)
 
     def test_403_on_protected_endpoints_without_cookie(self):
-        apps = self.create_apps()
-        for app in [apps['private'], apps['secret']]:
+        apps = create_apps()[0]
+        for app in [apps['private'], apps['public']]:
             app.layout = html.Div()
             client = app.server.test_client()
-            for endpoint in endpoints['unprotected']['get']:
-                res = client.get(endpoint)
-                self.assertEqual(res.status_code, 200, endpoint)
             for endpoint in endpoints['protected']['get']:
                 res = client.get(endpoint)
                 self.assertEqual(res.status_code, 403, endpoint)
+
+            for endpoint in endpoints['unprotected']['get']:
+                res = client.get(endpoint)
+                self.assertEqual(res.status_code, 200, endpoint)
 
             # TODO - check 200 on post of unprotected endpoints?
             for endpoint in endpoints['protected']['post']:
                 res = client.post(endpoint)
                 self.assertEqual(res.status_code, 403, endpoint)
 
-    def check_endpoints(self, auth, oauth_token, cookies=[], all_200=False):
+    def check_endpoints(self, auth, app, oauth_token, cookies=tuple(),
+                        all_200=False):
         def get_client():
             client = app.server.test_client()
             client.set_cookie(
@@ -133,86 +155,98 @@ class ProtectedViewsTest(unittest.TestCase):
                          endpoints['protected']['get']):
             client = get_client()  # use a fresh client for every endpoint
             res = client.get(endpoint)
-            test_name = '{} at {} as {} on {}'.format(
-                res.status_code, endpoint, oauth_token, app.fid
+            test_name = '{} at {} as {} on {} ({})'.format(
+                res.status_code, endpoint, oauth_token, auth._fid, auth._sharing
             )
-            if (auth.fid is None or
-                    auth.sharing == 'public' or
+            if (auth._fid is None or
+                    auth._sharing == 'public' or
                     oauth_token == users['creator']['oauth_token'] or
                     endpoint in endpoints['unprotected']['get'] or
                     all_200):
                 self.assertEqual(res.status_code, 200, test_name)
-            elif auth.sharing in ['private', 'secret']:
+            elif auth._sharing in ['private']:
                 self.assertEqual(res.status_code, 403, test_name)
         return res
 
     def test_protected_endpoints_with_auth_cookie(self):
-        apps, auths = self.create_apps()
-        for user_type, user_attributes in iteritems(users):
+        apps, auths = create_apps()
+        for user_attributes in users.values():
             for app_name, app in iteritems(apps):
-                app.layout = html.Div()
-                self.check_endpoints(
-                    auths[app_name],
-                    user_attributes['oauth_token'],
-                )
+                if app_name != 'unregistered':
+                    app.layout = html.Div()
+                    self.check_endpoints(
+                        auths[app_name],
+                        app,
+                        user_attributes['oauth_token'],
+                    )
 
     def test_permissions_can_change(self):
-        fn = 'private-flip-flop-app-test'
-        au = 'http://localhost:5000'
+        app_name = 'private-flip-flop-app-test'
+        app_url = 'http://localhost:5000'
         app = dash.Dash()
-        auth = PlotlyAuth(app, fn, 'private', au)
+        app.scripts.config.serve_locally = True
+        auth = PlotlyAuth(app, app_name, 'private', app_url)
         app.layout = html.Div()
-        auth.config.permissions_cache_expiry = 30
-        app.create_access_codes()
-        viewer = users['viewer']['oauth_token']
+        auth.config['permissions_cache_expiry'] = 30
+        auth.create_access_codes()
+        viewer_token = users['viewer']['oauth_token']
+        with mock.patch('dash_auth.plotly_auth.check_view_access',
+                        wraps=plotly_auth.check_view_access) as \
+                wrapped:
 
-        creator = users['creator']['oauth_token']
-        with mock.patch('dash_auth.check_view_access',
-                        wraps=dash_auth.check_view_access) as wrapped:
+            n_endpoints = (
+                len(endpoints['protected']['get']) +
+                len(endpoints['unprotected']['get']))
+
             # sanity check the endpoints when the app is private
-            self.check_endpoints(app, viewer)
-            self.assertEqual(wrapped.call_count, n_protected_endpoints)
+            self.check_endpoints(auth, app, viewer_token)
+            self.assertEqual(wrapped.call_count, n_endpoints)
 
-            # make the app public
-            dash_auth.create_or_overwrite_dash_app(fn, 'public', au)
-            auth.sharing = 'public'  # used in the check_endpoints assertions
-            res = self.check_endpoints(auth, viewer)
-            self.assertEqual(wrapped.call_count, n_protected_endpoints * 2)
+            # make app public
+            dash_auth.plotly_auth.create_or_overwrite_dash_app(
+                app_name, 'public', app_url
+            )
+            res = self.check_endpoints(auth, app, viewer_token, all_200=True)
+            self.assertEqual(wrapped.call_count, n_endpoints * 2)
 
             # The last access granted response contained a cookie that grants
             # the user access for 30 seconds (5 minutes by default)
             # without making an API call to plotly.
             # Include this cookie in the response and verify that it grants
             # the user access up until the expiration date
-            access_granted_cookie = get_cookie(res, 'dash_access')
+            access_granted_cookie = get_cookie(res, plotly_auth.AUTH_COOKIE_NAME)
             self.assertEqual(
                 access_granted_cookie,
-                app.access_codes['access_granted']
+                auth._access_codes['access_granted']
             )
-            self.assertEqual(wrapped.call_count, n_protected_endpoints * 2)
 
-            dash_auth.create_or_overwrite_dash_app(fn, 'private', au)
-            dash_auth.sharing = 'private'  # used in the check_endpoints assertions
-            # Even though the app is private, the viewer will still get 200s
-            access_cookie = [
-                {'name': auth.auth_cookie_name, 'value': access_granted_cookie}
-            ]
-            self.check_endpoints(
-                auth, viewer, access_cookie, all_200=True
+            plotly_auth.create_or_overwrite_dash_app(
+                app_name, 'private', app_url
             )
-            self.assertEqual(wrapped.call_count, n_protected_endpoints * 2)
+
+            # Even though the app is private, the viewer will still get 200s
+            access_cookie = (
+                {'name': plotly_auth.AUTH_COOKIE_NAME,
+                 'value': access_granted_cookie},
+            )
+            self.check_endpoints(
+                auth, app, viewer_token, access_cookie, all_200=True
+            )
+            self.assertEqual(wrapped.call_count, n_endpoints * 2)
 
             # But after 30 seconds, the auth token will expire,
             # and the user will be denied access
             time.sleep(5)
-            self.check_endpoints(auth, viewer, access_cookie, all_200=True)
-            self.assertEqual(wrapped.call_count, n_protected_endpoints * 2)
+            self.check_endpoints(auth, app, viewer_token,
+                                 access_cookie, all_200=True)
+            self.assertEqual(wrapped.call_count, n_endpoints * 2)
             time.sleep(26)
-            self.check_endpoints(auth, viewer, access_cookie)
-            self.assertEqual(wrapped.call_count, n_protected_endpoints * 3)
+            self.check_endpoints(auth, app, viewer_token, access_cookie)
+            self.assertEqual(wrapped.call_count, n_endpoints * 3)
 
     def test_auth_cookie_caches_calls_to_plotly(self):
         app = dash.Dash()
+        app.scripts.config.serve_locally = True
         auth = PlotlyAuth(
             app,
             'private-cookie-test',
@@ -222,47 +256,66 @@ class ProtectedViewsTest(unittest.TestCase):
         app.layout = html.Div()
 
         creator = users['creator']['oauth_token']
-        with mock.patch('dash_auth.check_view_access',
-                        wraps=dash_auth.check_view_access) as wrapped:
-            self.check_endpoints(app, creator)
-            res = self.check_endpoints(app, creator)
-            self.assertEqual(wrapped.call_count, 2 * n_protected_endpoints)
+        with mock.patch('dash_auth.plotly_auth.check_view_access',
+                        wraps=plotly_auth.check_view_access) as wrapped:
+            self.check_endpoints(auth, app, creator)
+            res = self.check_endpoints(auth, app, creator)
 
-            access_granted_cookie = get_cookie(res, 'dash_access')
-            self.check_endpoints(app, creator, [
-                {'name': app.auth_cookie_name, 'value': access_granted_cookie}
-            ])
-            self.assertEqual(wrapped.call_count, 2 * n_protected_endpoints)
+            n_endpoints = (
+                len(endpoints['protected']['get']) +
+                len(endpoints['unprotected']['get']))
+
+            self.assertEqual(wrapped.call_count, n_endpoints * 2)
+
+            access_granted_cookie = get_cookie(
+                res,
+                plotly_auth.AUTH_COOKIE_NAME)
+            self.check_endpoints(auth, app, creator, (
+                {'name': plotly_auth.AUTH_COOKIE_NAME,
+                 'value': access_granted_cookie},
+            ))
+            self.assertEqual(wrapped.call_count, n_endpoints * 2)
 
             # Regenerate tokens with a shorter expiration
             # User's won't actually do this in practice, we're
             # just doing it to shorten up the expiration from 5 min
             # to 10 seconds
-            auth.config.permissions_cache_expiry = 10
+            auth.config['permissions_cache_expiry'] = 10
             auth.create_access_codes()
-            res = self.check_endpoints(app, creator)
-            self.assertEqual(wrapped.call_count, 3 * n_protected_endpoints)
+            res = self.check_endpoints(auth, app, creator)
+            self.assertEqual(wrapped.call_count,
+                             n_endpoints * 3)
 
             # Using the same auth cookie should prevent an
             # additional access call
-            time.sleep(5)
-            access_granted_cookie = get_cookie(res, 'dash_access')
-            self.check_endpoints(auth, creator, [
-                {'name': auth.auth_cookie_name, 'value': access_granted_cookie}
-            ])
-            self.assertEqual(wrapped.call_count, 3 * n_protected_endpoints)
+            access_granted_cookie = get_cookie(res, plotly_auth.AUTH_COOKIE_NAME)
+            self.check_endpoints(auth, app, creator, (
+                {'name': plotly_auth.AUTH_COOKIE_NAME,
+                 'value': access_granted_cookie},
+            ))
+            self.assertEqual(
+                wrapped.call_count,
+                (n_endpoints * 3))
 
             # But after the expiration time (10 seconds), another call to
             # plotly should be made
-            time.sleep(6)
-            res = self.check_endpoints(auth, creator)
-            self.assertEqual(wrapped.call_count, 4 * n_protected_endpoints)
+            time.sleep(10)
+            self.check_endpoints(auth, app, creator)
+            self.assertEqual(
+                wrapped.call_count,
+                (n_endpoints * 4))
 
 
 class LoginFlow(unittest.TestCase):
     def login_success(self):
         app = dash.Dash()
-        auth = PlotlyAuth(app, 'test-auth-login-flow', 'private')
+        app.config.scripts.serve_locally = True
+        auth = PlotlyAuth(
+            app,
+            'test-auth-login-flow',
+            'private',
+            'https://dash-auth-app.herokuapp.com'
+        )
         app.layout = html.Div()
         client = app.server.test_client()
         csrf_token = get_cookie(client.get('/'), '_csrf_token')
@@ -275,5 +328,5 @@ class LoginFlow(unittest.TestCase):
         self.assertEqual(res.status_code, 200)
         self.assertEqual(
             get_cookie(res, 'plotly_oauth_token'),
-            token
+            oauth_token
         )
