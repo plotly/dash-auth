ecb656811d4c98e611197c970e0311aae3f6f6ac
chriddyp <chris@plot.ly>
2017-07-25 14:13:45 -0400

    pep8, code style, remove secret

--- dash_auth/plotly_auth.py
+++ dash_auth/plotly_auth.py
@@ -6,31 +6,35 @@ import os
 import plotly
 import requests
 from six import iteritems
-
 from auth import Auth
 
 
 AUTH_COOKIE_NAME = 'plotly_auth'
 
+
 class PlotlyAuth(Auth):
     def __init__(self, app, app_name, sharing, app_url):
         Auth.__init__(self, app)
 
-        self.fid = create_or_overwrite_dash_app(
+        self.config = {
+            'permissions_cache_expiry': 5 * 60
+        }
+
+        self._fid = create_or_overwrite_dash_app(
             app_name, sharing, app_url
         )
-        self.oauth_client_id = create_or_overwrite_oauth_app(
+        self._oauth_client_id = create_or_overwrite_oauth_app(
             app_url, app_name
         )['client_id']
 
-        self.app_url = app_url
-        self.sharing = sharing
-        self.access_codes = self.create_access_codes()
-        self.plotly_domain = 'https://plot.ly'
+        self._app_url = app_url
+        self._sharing = sharing
+        self._access_codes = self.create_access_codes()
+        self._plotly_domain = plotly.tools.get_config_file()['plotly_domain']
 
         app.server.add_url_rule(
             '{}_dash-login'.format(app.url_base_pathname),
-            view_func=self.login_api,
+            view_func=login_api,
             methods=['post']
         )
 
@@ -48,12 +52,10 @@ class PlotlyAuth(Auth):
         _current_path = os.path.dirname(os.path.abspath(__file__))
 
         # TODO - Dist files
-        self.oauth_redirect_bundle = open(os.path.join(_current_path, 'oauth-redirect.js')).read()
-        self.login_bundle = open(os.path.join(_current_path, 'login.js')).read()
-
-
-    class config:
-        permissions_cache_expiry = 5 * 60
+        self.oauth_redirect_bundle = open(os.path.join(
+            _current_path, 'oauth-redirect.js')).read()
+        self.login_bundle = open(
+            os.path.join(_current_path, 'login.js')).read()
 
     def create_access_codes(self):
         token = SeaSurf()._generate_token()
@@ -61,12 +63,12 @@ class PlotlyAuth(Auth):
             'access_granted': token,
             'expiration': (
                 datetime.datetime.now() + datetime.timedelta(
-                    seconds=self.config.permissions_cache_expiry
+                    seconds=self.config['permissions_cache_expiry']
                 )
             )
         }
-        self.access_codes = new_access_codes
-        return self.access_codes
+        self._access_codes = new_access_codes
+        return self._access_codes
 
     def is_authorized(self):
         if 'plotly_oauth_token' not in flask.request.cookies:
@@ -74,41 +76,42 @@ class PlotlyAuth(Auth):
 
         oauth_token = flask.request.cookies['plotly_oauth_token']
 
-        if (datetime.datetime.now() > self.access_codes['expiration']):
-            create_access_codes()
+        if (datetime.datetime.now() > self._access_codes['expiration']):
+            self.create_access_codes()
 
         if AUTH_COOKIE_NAME not in flask.request.cookies:
-            return check_view_access(oauth_token, self.fid)
-        else:
-            access_cookie = flask.request.cookies[AUTH_COOKIE_NAME]
+            return check_view_access(oauth_token, self._fid)
 
-            # If there access was previously declined,
-            # check access again in case it has changed
-            if access_cookie != self.access_codes['access_granted']:
-                return check_view_access(oauth_token, self.fid)
-            else:
-                return True
+        access_cookie = flask.request.cookies[AUTH_COOKIE_NAME]
+
+        # If there access was previously declined,
+        # check access again in case it has changed
+        if access_cookie != self._access_codes['access_granted']:
+            return check_view_access(oauth_token, self._fid)
+
+        return True
 
     def check_if_authorized(self):
         if self.is_authorized():
             return flask.Response(status=200)
-        else:
-            return flask.Response(status=403)
+
+        return flask.Response(status=403)
+
 
     def auth_wrapper(self, f):
         def wrap(*args, **kwargs):
             if not self.is_authorized():
                 return flask.Response(status=403)
-            else:
-                response = f(*args, **kwargs)
-                # TODO - should set secure in this cookie, not exposed in flask
-                # TODO - should set path or domain
-                response.set_cookie(
-                    AUTH_COOKIE_NAME,
-                    value=self.access_codes['access_granted'],
-                    max_age=(60 * 60 * 24 * 7),  # 1 week
-                )
-                return response
+
+            response = f(*args, **kwargs)
+            # TODO - should set secure in this cookie, not exposed in flask
+            # TODO - should set path or domain
+            response.set_cookie(
+                AUTH_COOKIE_NAME,
+                value=self._access_codes['access_granted'],
+                max_age=(60 * 60 * 24 * 7),  # 1 week
+            )
+            return response
         return wrap
 
     def html(self, script):
@@ -129,8 +132,8 @@ class PlotlyAuth(Auth):
             </html>
         '''.format(
             json.dumps({
-                'oauth_client_id': self.oauth_client_id,
-                'plotly_domain': self.plotly_domain
+                'oauth_client_id': self._oauth_client_id,
+                'plotly_domain': self._plotly_domain
             }),
             script)
         )
@@ -138,35 +141,34 @@ class PlotlyAuth(Auth):
     def login_html(self):
         return self.html(self.login_bundle)
 
-    def login_api(self):
-        oauth_token = flask.request.get_json()['access_token']
-        print('login_api')
-        print(oauth_token)
-        res = requests.get(
-            '{}/v2/users/current'.format(
-                plotly.config.get_config()['plotly_api_domain']
-            ),
-            headers={
-                'Authorization': 'Bearer {}'.format(oauth_token)
-            }
-        )
-        res.raise_for_status()
-        response = flask.Response(
-            json.dumps(res.json()),
-            mimetype='application/json',
-            status=res.status_code
-        )
-        # TODO - set path and secure appropriately
-        response.set_cookie(
-            'plotly_oauth_token',
-            value=oauth_token,
-            max_age=None
-        )
-
-        return response
-
     def serve_oauth_redirect(self):
         return self.html(self.oauth_redirect_bundle)
+
+
+def login_api():
+    oauth_token = flask.request.get_json()['access_token']
+    res = requests.get(
+        '{}/v2/users/current'.format(
+            plotly.config.get_config()['plotly_api_domain']
+        ),
+        headers={
+            'Authorization': 'Bearer {}'.format(oauth_token)
+        }
+    )
+    res.raise_for_status()
+    response = flask.Response(
+        json.dumps(res.json()),
+        mimetype='application/json',
+        status=res.status_code
+    )
+    # TODO - set path and secure appropriately
+    response.set_cookie(
+        'plotly_oauth_token',
+        value=oauth_token,
+        max_age=None
+    )
+
+    return response
 
 
 def create_or_overwrite_dash_app(filename, sharing, app_url):
@@ -178,10 +180,10 @@ def create_or_overwrite_dash_app(filename, sharing, app_url):
     for arg_name, arg_value in iteritems(required_args):
         if arg_value is None:
             raise Exception('{} is required'.format(arg_name))
-    if sharing not in ['private', 'secret', 'public']:
+    if sharing not in ['private', 'public']:
         raise Exception(
             "The privacy argument must be equal "
-            "to 'private', 'public', or 'secret'.\n"
+            "to 'private' or 'public'.\n"
             "You supplied '{}'".format(sharing)
         )
     payload = {
@@ -207,40 +209,73 @@ def create_or_overwrite_dash_app(filename, sharing, app_url):
         res.raise_for_status()
     return fid
 
+
 def create_or_overwrite_oauth_app(app_url, name):
+    # TODO - ENV for creds?
+    creds = plotly.tools.get_credentials_file()
+    config = plotly.tools.get_config_file()
 
     redirect_uris = [
         '{}/_oauth-redirect'.format(i) for i in [
-            # TODO - windows does localhost?
-            # TODO - variable port?
+            # TODO - variable or app.server.settings port
             'http://localhost:8050',
             app_url
         ]
     ]
-
-    creds = plotly.tools.get_credentials_file()
-
-    # TODO - Use lookup
-    res = requests.post(
-        'https://api.plot.ly/v2/oauth-apps',
-        auth=(creds['username'], creds['api_key'],),
-        headers={'plotly-client-platform': 'python'},
-        data={
+    auth = (creds['username'], creds['api_key'],)
+    headers = {
+        'plotly-client-platform': 'dash-auth',
+        'content-type': 'application/json'
+    }
+    request_parameters = {
+        'data': json.dumps({
             'name': name,
             'client_type': 'public',
             'authorization_grant_type': 'implicit',
             'redirect_uris': ' '.join(redirect_uris),
-        }
+        }),
+        'headers': headers,
+        'auth': auth
+    }
+
+    # Check if app already exists.
+    # If it does, then update it.
+    # If it doesn't, then create a new one.
+    res = requests.get(
+        '{}/v2/oauth-apps/lookup'.format(config['plotly_api_domain']),
+        auth=auth,
+        headers=headers,
+        params={'name': name}
     )
+    res.raise_for_status()
+    apps = res.json()
+    if len(apps) > 1:
+        raise Exception(
+            'There are more than one oauth apps with the name {}.'.format(name)
+        )
+    elif len(apps) == 1:
+        oauth_app_id = apps[0]['id']
+        res = requests.patch(
+            '{}/v2/oauth-apps/{}'.format(
+                config['plotly_api_domain'],
+                oauth_app_id
+            ),
+            **request_parameters
+        )
+    else:
+        res = requests.post(
+            '{}/v2/oauth-apps'.format(config['plotly_api_domain']),
+            **request_parameters
+        )
 
     res.raise_for_status()
-
     return res.json()
+
 
 def check_view_access(oauth_token, fid):
     res = requests.get(
         '{}/v2/files/{}'.format(
-            plotly.config.get_config()['plotly_api_domain'],
+            plotly.tools.get_config_file()['plotly_api_domain'],
             fid
         ),
         headers={
