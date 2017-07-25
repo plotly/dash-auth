1698c3b7f8f1ad83a151492e41457736bb24f018
chriddyp <chris@plot.ly>
2017-07-25 18:24:15 -0400

    basic auth

---
+++ dash_auth/basic_auth.py
@@ -0,0 +1,36 @@
+from auth import Auth
+import base64
+import flask
+
+
+class BasicAuth(Auth):
+    def __init__(self, app, username_password_list):
+        Auth.__init__(self, app)
+        self._username_password_list = username_password_list
+
+    def is_authorized(self):
+        header = flask.request.headers.get('Authorization', None)
+        if not header:
+            return False
+        username, password = base64.b64decode(
+            header.split('Basic ')[1]).split(':')
+        for pair in self._username_password_list:
+            if pair[0] == username and pair[1] == password:
+                return True
+
+        return False
+
+    def login_request(self):
+        return flask.Response(
+            'Login Required',
+            headers={'WWW-Authenticate': 'Basic realm="User Visible Realm"'},
+            status=401)
+
+    def auth_wrapper(self, f):
+        def wrap(*args, **kwargs):
+            if not self.is_authorized():
+                return flask.Response(status=403)
+
+            response = f(*args, **kwargs)
+            return response
+        return wrap
