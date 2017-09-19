76a7aa88ce6fc2eafeff34ef086a1b8b76b19a39
chriddyp <chris@plot.ly>
2017-09-19 16:37:35 -0400

    the view name for dash's index is no longer `index`
    this was changed to allow multiple apps to run with different
    `routes_pathname_prefix`s - https://github.com/plotly/dash/pull/70/files

--- dash_auth/auth.py
+++ dash_auth/auth.py
@@ -11,6 +11,7 @@ class Auth(object):
         self.app = app
         self._overwrite_index()
         self._protect_views()
+        self._index_view_name = app.config['routes_pathname_prefix']
 
     def _overwrite_index(self):
         original_index = self.app.server.view_functions['index']
@@ -21,7 +22,7 @@ class Auth(object):
             else:
                 return self.login_request()
 
-        self.app.server.view_functions['index'] = wrap_index
+        self.app.server.view_functions[self._index_view_name] = wrap_index
 
     def _protect_views(self):
         # TODO - allow users to white list in case they add their own views
