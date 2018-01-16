3666ba3c29fbbba7be4b1e4a64f9e96c1ae8d9ab
jn7dt <jn7dt@virginia.edu>
2018-01-16 17:09:30 -0500

    Added HashAuth class with ability to match user/pass with sha hashed environment variables.

--- dash_auth/basic_auth.py
+++ dash_auth/basic_auth.py
@@ -2,6 +2,8 @@ from .auth import Auth
 import base64
 import flask
 
+import hashlib
+
 
 class BasicAuth(Auth):
     def __init__(self, app, username_password_list):
@@ -17,6 +19,49 @@ class BasicAuth(Auth):
         username, password = username_password_utf8.split(':')
         for pair in self._username_password_list:
             if pair[0] == username and pair[1] == password:
+                return True
+
+        return False
+
+    def login_request(self):
+        return flask.Response(
+            'Login Required',
+            headers={'WWW-Authenticate': 'Basic realm="User Visible Realm"'},
+            status=401)
+
+    def auth_wrapper(self, f):
+        def wrap(*args, **kwargs):
+            if not self.is_authorized():
+                return flask.Response(status=403)
+
+            response = f(*args, **kwargs)
+            return response
+        return wrap
+
+class HashAuth(Auth):
+
+    def __init__(self, app, username_password_list, hash_type='sha256'):
+        Auth.__init__(self, app)
+        self._username_password_list = username_password_list
+        self.hash_type = hash_type
+
+    def is_authorized(self):
+        header = flask.request.headers.get('Authorization', None)
+        if not header:
+            return False
+        username_password = base64.b64decode(header.split('Basic ')[1])
+        username_password_utf8 = username_password.decode('utf-8')
+        username, password = username_password_utf8.split(':')
+
+        user_hasher = hashlib.new(self.hash_type)
+        pwd_hasher = hashlib.new(self.hash_type)
+        user_hasher.update(username.encode())
+        pwd_hasher.update(password.encode())
+        hash_user = user_hasher.hexdigest()
+        hash_pwd = pwd_hasher.hexdigest()
+
+        for pair in self._username_password_list:
+            if pair[0] == hash_user and pair[1] == hash_pwd:
                 return True
 
         return False
