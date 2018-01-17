df06bee75318ff04fcbe070ad945eb4642fc0f88
jn7dt <jn7dt@virginia.edu>
2018-01-17 10:31:04 -0500

    Refactored auth classes to remove independent HashAuth class and instead include optional argument to BasicAuth that handles hashing of username and password.

--- dash_auth/__init__.py
+++ dash_auth/__init__.py
@@ -1,4 +1,4 @@
 from __future__ import absolute_import
 from .plotly_auth import PlotlyAuth  # noqa: F401
-from .basic_auth import BasicAuth, HashAuth  # noqa: F401
+from .basic_auth import BasicAuth  # noqa: F401
 from .version import __version__  # noqa: F401
--- dash_auth/basic_auth.py
+++ dash_auth/basic_auth.py
@@ -1,14 +1,14 @@
 from .auth import Auth
 import base64
 import flask
-
 import hashlib
 
 
 class BasicAuth(Auth):
-    def __init__(self, app, username_password_list):
+    def __init__(self, app, username_password_list, hash_type=None):
         Auth.__init__(self, app)
         self._username_password_list = username_password_list
+        self._hash_type = hash_type
 
     def is_authorized(self):
         header = flask.request.headers.get('Authorization', None)
@@ -17,51 +17,11 @@ class BasicAuth(Auth):
         username_password = base64.b64decode(header.split('Basic ')[1])
         username_password_utf8 = username_password.decode('utf-8')
         username, password = username_password_utf8.split(':')
+        if self._hash_type:
+            username = hashlib.new(self._hash_type, username.encode()).hexdigest()
+            password = hashlib.new(self._hash_type, password.encode()).hexdigest()
         for pair in self._username_password_list:
             if pair[0] == username and pair[1] == password:
-                return True
-
-        return False
-
-    def login_request(self):
-        return flask.Response(
-            'Login Required',
-            headers={'WWW-Authenticate': 'Basic realm="User Visible Realm"'},
-            status=401)
-
-    def auth_wrapper(self, f):
-        def wrap(*args, **kwargs):
-            if not self.is_authorized():
-                return flask.Response(status=403)
-
-            response = f(*args, **kwargs)
-            return response
-        return wrap
-
-class HashAuth(Auth):
-
-    def __init__(self, app, username_password_list, hash_type='sha256'):
-        Auth.__init__(self, app)
-        self._username_password_list = username_password_list
-        self.hash_type = hash_type
-
-    def is_authorized(self):
-        header = flask.request.headers.get('Authorization', None)
-        if not header:
-            return False
-        username_password = base64.b64decode(header.split('Basic ')[1])
-        username_password_utf8 = username_password.decode('utf-8')
-        username, password = username_password_utf8.split(':')
-
-        user_hasher = hashlib.new(self.hash_type)
-        pwd_hasher = hashlib.new(self.hash_type)
-        user_hasher.update(username.encode())
-        pwd_hasher.update(password.encode())
-        hash_user = user_hasher.hexdigest()
-        hash_pwd = pwd_hasher.hexdigest()
-
-        for pair in self._username_password_list:
-            if pair[0] == hash_user and pair[1] == hash_pwd:
                 return True
 
         return False
