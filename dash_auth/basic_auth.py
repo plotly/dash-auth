5b44300aef80093391b4a1c7b78be5572736196f
jn7dt <jn7dt@virginia.edu>
2018-01-17 10:43:14 -0500

    Improved reliability of hash_type option by including list of valid hash types.

--- dash_auth/basic_auth.py
+++ dash_auth/basic_auth.py
@@ -5,6 +5,12 @@ import hashlib
 
 
 class BasicAuth(Auth):
+
+    VALID_HASH_TYPES = [
+        'sha1','sha224','sha256','sha384','sha512','blake2b','blake2s',
+        'md5','sha3_224','sha3_256','sha3_512','shake_128','shake_256'
+    ]
+
     def __init__(self, app, username_password_list, hash_type=None):
         Auth.__init__(self, app)
         self._username_password_list = username_password_list
@@ -17,7 +23,7 @@ class BasicAuth(Auth):
         username_password = base64.b64decode(header.split('Basic ')[1])
         username_password_utf8 = username_password.decode('utf-8')
         username, password = username_password_utf8.split(':')
-        if self._hash_type:
+        if self._hash_type in VALID_HASH_TYPES:
             username = hashlib.new(self._hash_type, username.encode()).hexdigest()
             password = hashlib.new(self._hash_type, password.encode()).hexdigest()
         for pair in self._username_password_list:
