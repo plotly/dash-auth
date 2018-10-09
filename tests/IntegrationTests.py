514297ad8d10454a27e0e1f663c364e3e96cd835
Philippe Duval <philippe@plot.ly>
2018-10-09 18:26:21 -0400

    Timeout 20.

--- tests/IntegrationTests.py
+++ tests/IntegrationTests.py
@@ -12,7 +12,7 @@ from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-TIMEOUT = 10
+TIMEOUT = 20
 
 
 class IntegrationTests(unittest.TestCase):
